7636bc589672d29f505b1c322318628bbd150d83
James Gonsalves <jgon0201@gmail.com>
2023-04-19 17:08:46 -0400

    policy changes

--- final-project-web-ui/deploy/create-bucket.sh
+++ final-project-web-ui/deploy/create-bucket.sh
@@ -1,14 +1,14 @@
-aws s3api create-bucket --bucket sample-bucket-dn --acl public-read
-aws s3 website s3://sample-bucket-dn --index-document index.html
+aws s3api create-bucket --bucket csc375-jg1 --acl public-read
+aws s3 website s3://csc375-jg1 --index-document index.html
 
 # Download the zip file from S3, note the use of the S3 URI, not HTTPS
-aws s3 cp s3://sample-bucket-dn/build.zip build.zip
+aws s3 cp s3://csc375-jg1/application.zip application.zip
 
 # Decompress the zip file into a temp directory
-unzip -d web_ui_deploy build.zip
+unzip -d web_ui_deploy application.zip
 
 # Sync up the contents of the temp directory to S3 prefix
-aws s3 sync web_ui_deploy s3://sample-bucket-dn
+aws s3 sync web_ui_deploy s3://csc375-jg1
 
 # And optionally, clean up the temp files and directory
 # Unix:
